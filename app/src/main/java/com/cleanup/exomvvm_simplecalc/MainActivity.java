ecf0a4b9e5cc881eada775f9d8fc85c5941a8c94
Jerome Rigault <jerome.rigault@expleogroup.com>
2021-06-10 15:20:46 +0200

    Initial commit

---
+++ app/src/main/java/com/cleanup/exomvvm_simplecalc/MainActivity.java
@@ -0,0 +1,21 @@
+package com.cleanup.exomvvm_simplecalc;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.cleanup.exomvvm_simplecalc.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
